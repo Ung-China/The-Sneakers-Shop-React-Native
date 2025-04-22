a0b95ff52322a1aacaedd030efb08541124bf72f
Ing-China <peschina6677@gmail.com>
2025-04-22 23:35:32 +0700

    Change api url

--- src/api/Requests/index.ts
+++ src/api/Requests/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {ApiRequestProps} from '../../types';
 
 const apiInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://thesneakersshop.site/api',
   timeout: 5000,
 });
 
